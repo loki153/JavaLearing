b416a7b37ca42bb5405741c5e7f6d2841adc04e6
loki153 <1047668416@qq.com>
2019-12-11 14:09:06 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Loki
+ * @Date: 2019/12/11 14:04
+ * @Project: JavaLearing
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
