497eb890dea999cbe8a87f91fc82b8f77e54ce53
loki153 <1047668416@qq.com>
2019-12-11 14:23:41 +0800

    modify

--- src/Hello.java
+++ src/Hello.java
@@ -7,5 +7,7 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("hello world!");
+        System.out.println("fool!");
+        System.out.println("hooo");
     }
 }
