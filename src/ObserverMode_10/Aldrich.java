d8c06619af7b70d5e70b9e22f9f073bd6985db5d
loki153 <1047668416@qq.com>
2020-04-15 22:47:55 +0800

    updated ObserverMode_9

---
+++ src/ObserverMode_10/Aldrich.java
@@ -0,0 +1,28 @@
+package ObserverMode_10;
+
+public class Aldrich  extends  Cinder{
+    private String name;
+    private String FireState;
+    private Fire fire;
+
+
+    public Aldrich (Fire fire,String name ){
+        this.fire = fire;
+        this.name = name;
+    }
+
+    public Fire getFire() {
+        return fire;
+    }
+
+    public void setFire(Fire fire) {
+        this.fire = fire;
+    }
+
+    @Override
+    public void react() {
+        System.out.println(this.name + "：预见到深海时代的来临，开始了吞噬神明的道路");
+    }
+
+
+}
---
+++ src/ObserverMode_10/Cinder.java
@@ -0,0 +1,5 @@
+package ObserverMode_10;
+
+public abstract class Cinder {
+    public abstract void react();
+}
